52908d0a5b445e39de6fdc3d6c27c92b47ebef0e
Tanner Winkelman <21373626+winkelmantanner@users.noreply.github.com>
2018-07-17 21:08:56 -0500

    Refactor code.

--- threadedQuickSort.h
+++ threadedQuickSort.h
@@ -37,22 +37,18 @@ void quickSortRecursive( T *const arr, const long min, const long max, const sho
   if( numThreads == 1 ){
     quickSortRecursive( arr, min, pivotIndex - 1, 1 );
     quickSortRecursive( arr, pivotIndex + 1, max, 1 );
-  }else if( numThreads == 2 ) {
-    thread f1 (&quickSortRecursive<T>, arr, min, pivotIndex - 1, 1 );
-    thread f2 (&quickSortRecursive<T>, arr, pivotIndex + 1, max, 1 );
-    f1.join();
-    f2.join();
-  }else{ // numThreads >= 3
-    long problemSize1 = pivotIndex - min;
-    long problemSize2 = max - pivotIndex;
-    short numThreadsOnFirstHalf = ((numThreads * problemSize1) + (problemSize2 >> 1)) / (problemSize2 + problemSize1);
-    
-    if( numThreadsOnFirstHalf <= 0 ) {
-      numThreadsOnFirstHalf = 1;
-    }else if( numThreadsOnFirstHalf >= numThreads - 1 ) {
-      numThreadsOnFirstHalf = numThreads - 1;
+  }else {
+    short numThreadsOnFirstHalf = 1;
+    if( numThreads > 2 ){
+      long problemSize1 = pivotIndex - min;
+      long problemSize2 = max - pivotIndex;
+      numThreadsOnFirstHalf = ((numThreads * problemSize1) + (problemSize2 >> 1)) / (problemSize2 + problemSize1);
+      if( numThreadsOnFirstHalf <= 0 ) {
+        numThreadsOnFirstHalf = 1;
+      }else if( numThreadsOnFirstHalf >= numThreads - 1 ) {
+        numThreadsOnFirstHalf = numThreads - 1;
+      }
     }
-    // I found that combining this with the threading above makes the program much slower
     thread t1 (&quickSortRecursive<T>, arr, min, pivotIndex - 1, numThreadsOnFirstHalf );
     thread t2 (&quickSortRecursive<T>, arr, pivotIndex + 1, max, numThreads - numThreadsOnFirstHalf );
     t1.join();
